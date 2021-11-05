1c4b0c642b243553ac26b931f6d63462d31ce63e
Micbush <bushmickel@gmail.com>
2021-11-05 10:26:29 +0100

    updated changes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
 
     // Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch('https://github.com/Micbush/tasktrackerapp/blob/master/db.json/tasks')
+    const res = await fetch('http://localhost:5000/tasks')
     const data = await res.json()
 
     return data
@@ -30,7 +30,7 @@ const App = () => {
 
   // Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(`https://github.com/Micbush/tasktrackerapp/blob/master/db.json/tasks/${id}`)
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json()
 
     return data
@@ -38,7 +38,7 @@ const App = () => {
 
   //  Add Task
   const addTask = async (task) => {
-    const res = await fetch('https://github.com/Micbush/tasktrackerapp/blob/master/db.json/tasks', {
+    const res = await fetch('http://localhost:5000/tasks', {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
@@ -56,7 +56,7 @@ const App = () => {
 
   //  Delete Task
   const deleteTask = async (id) => {
-    await fetch(`https://github.com/Micbush/tasktrackerapp/blob/master/db.json/tasks/${id}`, {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'DELETE',
     })
     setTasks(tasks.filter((task) => task.id !== id))
@@ -67,7 +67,7 @@ const App = () => {
     const taskToToggle = await fetchTask(id)
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-    const res = await fetch(`https://github.com/Micbush/tasktrackerapp/blob/master/db.json/tasks/${id}`, {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'PUT',
       headers: {
         'Content-type': 'application/json'
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
-import { useLocation } from 'react-router-dom'
+// import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 const Footer = () => {
-    const location = useLocation()
     return (
         <footer>
             <p>Copyright &copy; 2020</p>
-            {location.pathname === '/tasktrackerapp' && (< Link to='/about'>About</Link>)}
+            < Link to='/about'>About</Link>
         </footer>
     )
 }
