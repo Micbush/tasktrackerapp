696e6ca63170d7eb83cef8aeb52a80d84c9f5b3e
Micbush <bushmickel@gmail.com>
2021-11-05 10:23:16 +0100

    updated changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
+import { useLocation } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 const Footer = () => {
+    const location = useLocation()
     return (
         <footer>
             <p>Copyright &copy; 2020</p>
-            < Link to='/about'>About</Link>
+            {location.pathname === '/tasktrackerapp' && (< Link to='/about'>About</Link>)}
         </footer>
     )
 }
